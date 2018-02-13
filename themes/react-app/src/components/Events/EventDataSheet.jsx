39d7e160792c9ea8563b4985bb8c04a3a98f60b1
Dunatron <heath.dunlop.hd@gmail.com>
2018-02-14 01:17:34 +1300

    ff

--- themes/react-app/src/components/Events/EventDataSheet.jsx
+++ themes/react-app/src/components/Events/EventDataSheet.jsx
@@ -21,6 +21,8 @@ import {CircularProgress} from 'material-ui/Progress';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 import HappMap from '../HappMap';
 
+import EventCard from './EventCard';
+
 const customStyles = {
   content : {
     'width': 'max-content',
@@ -136,106 +138,7 @@ class EventDataSheet extends Component {
     console.groupEnd();
 
     return (
-      <div>
-        <Card className={classes.card}>
-          <CardHeader
-            avatar={
-              <Avatar aria-label="Recipe" className={classes.avatar}>
-                R
-              </Avatar>
-            }
-            action={
-              <IconButton>
-                <MoreVertIcon/>
-              </IconButton>
-            }
-            title={EventData.Title}
-            subheader={EventData.Date}
-          />
-          <CardMedia
-            className={classes.media}
-            image="http://via.placeholder.com/350x150"
-            title="Contemplative Reptile"
-          />
-          <CardContent>
-            <div className={classes.timeHolder}>
-              <TimeIcon/> {EventData.Start} - {EventData.Finish}
-            </div>
-            <div>
-              <LocationIcon/> {EventData.LocationText}
-            </div>
-            <Typography component="p">
-              {EventData.Description}
-            </Typography>
-          </CardContent>
-          <CardActions disableActionSpacing>
-            <IconButton aria-label="Add to favorites">
-              <FavoriteIcon/>
-            </IconButton>
-            <CopyToClipboard text={window.location.href} onCopy={this.onCopy}>
-              <IconButton aria-label="Share">
-                <ShareIcon/>
-              </IconButton>
-            </CopyToClipboard>
-            <div>
-              {this.state.copied ? <span >Copied.</span> : null}
-            </div>
-            <IconButton aria-label="show-map" onClick={this.toggleDrawer('bottomDraw', true)}>
-              <MapIcon/>
-            </IconButton>
-
-            <div className={classes.flexGrow}/>
-            <IconButton
-              className={classnames(classes.expand, {
-                [classes.expandOpen]: this.state.expanded,
-              })}
-              onClick={this.handleExpandClick}
-              aria-expanded={this.state.expanded}
-              aria-label="Show more"
-            >
-              <ExpandMoreIcon/>
-            </IconButton>
-          </CardActions>
-          <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
-            <CardContent>
-              <Typography paragraph type="body2">
-                Full Description:
-              </Typography>
-              <Typography paragraph>
-                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-                minutes.
-              </Typography>
-              <Typography paragraph>
-                Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-                browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving
-                chicken and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion,
-                salt and pepper, and cook, stirring often until thickened and fragrant, about 10
-                minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-              </Typography>
-              <Typography paragraph>
-                Add rice and stir very gently to distribute. Top with artichokes and peppers, and
-                cook without stirring, until most of the liquid is absorbed, 15 to 18 minutes.
-                Reduce heat to medium-low, add reserved shrimp and mussels, tucking them down into
-                the rice, and cook again without stirring, until mussels have opened and rice is
-                just tender, 5 to 7 minutes more. (Discard any mussels that don’t open.)
-              </Typography>
-              <Typography>
-                Set aside off of the heat to let rest for 10 minutes, and then serve.
-              </Typography>
-            </CardContent>
-          </Collapse>
-        </Card>
-        <Drawer
-          anchor="bottom"
-          open={this.state.bottomDraw}
-          onRequestClose={this.toggleDrawer('bottomDraw', false)}
-        >
-          <div>
-            <HappMap defaultZoom={15} lat={parseFloat(EventData.Lat)} lng={parseFloat(EventData.Lon)}/>
-          </div>
-        </Drawer>
-      </div>
+      <EventCard eventObject={EventData} />
     );
   }
 }
@@ -260,11 +163,27 @@ query getEventByIDQuery($eventID: ID!) {
     SpecEntry
     AccessType
     IsEventFindaEvent
-    EventFindaID
+    Thumbnail
     EventFindaUrl
+    EventFindaImages {
+     edges {
+       node {
+         URL
+         Title
+         transformation_id
+        }
+      }
+    }
     LocationText
     Lat
     Lon
+    Image {
+      ID
+     }
+    SecondaryTag {
+      ID
+      Title
+    }
   }
 }
 `;
